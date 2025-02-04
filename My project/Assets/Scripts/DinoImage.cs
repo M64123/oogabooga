14c6d03922f52c1b468c4b9affbfbc02a08b07f0
KoreanSpaceJam <163570106+KoreanSpaceJam@users.noreply.github.com>
2025-02-04 06:37:34 +0100

    Revert "Merge branch 'MergeaEsta' into arreglo-coinflip-y-camaras"
    This reverts commit a6589fe6fc58999519ed79867a446e82d5fc91cf, reversing
    changes made to b7467f2ff4fcf3123d3680f8b021ba07d9e08a9e.

--- My project/Assets/Scripts/DinoImage.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class DinoImage : MonoBehaviour
-{
-    public string dinoID; // ID del dinosaurio obtenido del GameManager
-    public string dinoName; // Nombre del dinosaurio obtenido del GameManager
-}
